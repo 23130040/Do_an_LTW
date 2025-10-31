513610aab2688a603376d781149e9e5c05634f31
nmhau2410 <23130096@st.hcmuaf.edu.vn>
2025-10-31 17:47:11 +0700

    Tạo trang admin và thêm giao diện quản lý sản phẩm

---
+++ JavaScript/admin_quan_ly_sp.js
@@ -0,0 +1,6 @@
+function openProductModal() {
+    document.getElementById('product-modal').style.display = 'flex';
+}
+function closeProductModal() {
+    document.getElementById('product-modal').style.display = 'none';
+}
