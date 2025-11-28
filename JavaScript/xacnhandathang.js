7627d9fb31ef84f364a36bfab0894ad7dd11f7a8
Nguyen Bich Dan <23130040@st.hcmuaf.edu.vn>
2025-11-28 16:51:13 +0700

    Chỉnh sửa màu, đồng bộ tiêu đề trên tất cả các trang, thêm modal chỉnh sửa thông tin trong trang xác nhận đặt hàng...

--- JavaScript/xacnhandathang.js
+++ JavaScript/xacnhandathang.js
@@ -47,7 +47,44 @@ document.addEventListener("DOMContentLoaded", () => {
         deliveryMessage.style.display = "none";
     });
 
+    /**Thay đổi địa chỉ nhận hàng*/
+    const editProfileModal = document.getElementById("edit-profile-modal");
+    const openEditProfileModal = document.getElementById("edit-profile");
+    openEditProfileModal.addEventListener("click", () => {
+        editProfileModal.style.display = "block";
+    });
+    const cancleProfileModal = document.getElementById("cancle-edit-profile-btn");
+    cancleProfileModal.addEventListener("click", () => {
+        editProfileModal.style.display = "none";
+    });
+    const confirmProfileModal = document.getElementById("confirm-edit-profile-btn");
+    confirmProfileModal.addEventListener("click", () => {
+        editProfileModal.style.display = "none";
+    });
+    const closeProfileModal = document.getElementById("close-edit-profile-modal");
+    closeProfileModal.addEventListener("click", () => {
+        editProfileModal.style.display = "none";
+    });
+    const openAddressModal = document.getElementById("add-address-btn");
+    openAddressModal.addEventListener("click", () => {
+        addAddressModal.style.display = "block";
+    });
+    const closeAddressModal = document.getElementById("close-add-address-modal");
+    closeAddressModal.addEventListener("click", () => {
+        addAddressModal.style.display = "none";
+    });
+    const confirmAddressModal = document.getElementById("confirm-add-address-btn");
+    confirmAddressModal.addEventListener("click", () => {
+        addAddressModal.style.display = "none";
+    });
+    const cancleAddressModal = document.getElementById("cancle-add-address-btn");
+    cancleAddressModal.addEventListener("click", () => {
+        addAddressModal.style.display = "none";
+    });
+    const addAddressModal = document.getElementById("add-address-modal");
     window.addEventListener('click', (e) => {
         if (e.target === deliveryMessage) deliveryMessage.style.display = "none";
+        if (e.target === editProfileModal) editProfileModal.style.display = "none";
+        if (e.target === addAddressModal) addAddressModal.style.display = "none";
     });
 });
